80f42ec83e101a01f2941a2c89100151cd0f7402
anhtt2211 <trananh22112001@gmail.com>
2023-05-19 16:19:53 +0700

    feat: initialize base structure for CustomerService

--- customer-service/src/app.service.ts
+++ customer-service/src/app.service.ts
@@ -5,4 +5,18 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  create(dto: any) {
+    console.log({ dto });
+    return 'Create customer';
+  }
+
+  confirmCustomer(dto: any) {
+    console.log({ dto });
+    return 'Confirm customer';
+  }
+
+  deleteCustomer(dto: any) {
+    console.log({ dto });
+    return 'Delete customer';
+  }
 }
--- customer-service/src/main.ts
+++ customer-service/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+  );
+
+  await app.listen();
 }
 bootstrap();
