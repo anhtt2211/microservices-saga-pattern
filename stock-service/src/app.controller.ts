7b021a93a9bd7af4555f3f3f23a9ad3e322fc73c
anhtt2211 <trananh22112001@gmail.com>
2023-05-19 16:22:53 +0700

    feat: init base structure StockService

--- stock-service/src/app.controller.ts
+++ stock-service/src/app.controller.ts
@@ -1,12 +1,71 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ClientProxy,
+  Ctx,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+
+    @Inject('SAGA_CLIENT')
+    private readonly sagaClient: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async create(@Body() createStockDto: any) {
+    const stockId = await this.appService.create(createStockDto);
+
+    // Publish a message to the Saga Coordinator to initiate the local transaction
+    const message = {
+      type: 'STOCK_CREATED',
+      payload: {
+        stockId,
+      },
+    };
+    await this.sagaClient.emit('saga', message);
+
+    return { stockId };
+  }
+
+  @MessagePattern('saga')
+  async handleSagaMessage(@Payload() message, @Ctx() context) {
+    const originalMessage = context.getMessage();
+    switch (message.type) {
+      case 'STOCK_CREATED':
+        try {
+          // Perform the local transaction
+          await this.appService.confirmStock(message.payload.stockId);
+          // Acknowledge the message
+          context.getChannelRef().ack(originalMessage);
+        } catch (e) {
+          // If the local transaction fails, initiate the compensating transaction
+          const compensatingMessage = {
+            type: 'STOCK_CONFIRMATION_FAILED',
+            payload: {
+              stockId: message.payload.stockId,
+            },
+          };
+          await this.sagaClient.emit('saga', compensatingMessage);
+          // Acknowledge the message
+          context.getChannelRef().ack(originalMessage);
+        }
+        break;
+      case 'STOCK_CONFIRMATION_FAILED':
+        // Undo the effects of the failed local transaction
+        await this.appService.deleteStock(message.payload.stockId);
+        // Acknowledge the message
+        context.getChannelRef().ack(originalMessage);
+        break;
+    }
   }
 }
--- stock-service/src/app.service.ts
+++ stock-service/src/app.service.ts
@@ -5,4 +5,19 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  create(dto: any) {
+    console.log({ dto });
+    return 'Create Stock';
+  }
+
+  confirmStock(dto: any) {
+    console.log({ dto });
+    return 'Confirm Stock';
+  }
+
+  deleteStock(dto: any) {
+    console.log({ dto });
+    return 'Delete Stock';
+  }
 }
