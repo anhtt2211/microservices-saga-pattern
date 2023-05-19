14d740edef8ed44644934abdd683041ab27a57a4
anhtt2211 <trananh22112001@gmail.com>
2023-05-19 15:52:54 +0700

    add: add docker to pull rabbitmq

--- saga-coordinator/src/main.ts
+++ saga-coordinator/src/main.ts
@@ -4,20 +4,21 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://guest:guest@rabbitmq:5672'],
-      queue: 'saga-coordinator',
-      queueOptions: {
-        durable: false,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        // urls: ['amqp://guest:guest@rabbitmq:5672'], // uncomment to run via docker
+        urls: ['amqp://localhost'], // uncomment to run via nodejs
+        queue: 'saga-coordinator',
+        queueOptions: {
+          durable: true,
+        },
       },
     },
-  });
+  );
 
-  await app.startAllMicroservices();
-  await app.listen(3000);
+  await app.listen();
 }
 bootstrap();
