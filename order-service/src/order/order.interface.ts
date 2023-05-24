04368f3827f51cdbd1584269c1e4191ea9df1a99
anhtt2211 <trananh22112001@gmail.com>
2023-05-24 14:43:11 +0700

    chore: remove Class unused

--- order-service/src/order/order.interface.ts
+++ order-service/src/order/order.interface.ts
@@ -1,17 +1,5 @@
 import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
 
-export class CreateOrderDto {
-  @IsNotEmpty()
-  customerId: number;
-
-  @IsNotEmpty()
-  stockId: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
-}
-
 export class PlaceOrderDto {
   @IsNotEmpty()
   customerId: number;
