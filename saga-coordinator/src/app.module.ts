d136eb06de38f09cdd2aecda762cba0df23fe9be
anhtt2211 <trananh22112001@gmail.com>
2023-05-25 11:38:26 +0700

    add: add config devtool on nestjs

--- saga-coordinator/src/app.module.ts
+++ saga-coordinator/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SagaCoordinatorModule } from './saga-coordinator/saga-coordinator.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-  imports: [SagaCoordinatorModule],
+  imports: [
+    DevtoolsModule.register({
+      http: true,
+    }),
+    SagaCoordinatorModule,
+  ],
 })
 export class AppModule {}
